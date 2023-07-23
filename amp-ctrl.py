c03afc14a46a9d2be5306122ffe1cfbba308cc79
Julian <julian.limburg@gmx.de>
2023-07-23 16:12:54 +0200

    amp logic

--- amp-ctrl.py
+++ amp-ctrl.py
@@ -22,8 +22,34 @@ def request_status():
     if x.status_code == 200:
         content = x.json()
         result = check_players_playing(content)
-        print(result)
+        return result
+    return False
+
+is_playing = False
+last_status_change_time = 0
 
 while True:
-    request_status()
+    new_status = request_status()
+
+    if new_status != is_playing:
+        last_status_change_time = time.time()
+
+    if new_status and not is_playing:
+        is_playing = True
+        # Start Amp here
+    elif not new_status and is_playing:
+        # Wait for 10 mins before stopping amp if nothing changes
+        time_since_change = time.time() - last_status_change_time
+        if time_since_change >= 600:  # 10 minutes = 600 seconds
+            is_playing = False
+            # Stop Amp here (Replace this comment with the actual code to stop the Amp)
+            print("Amp stopped.")
+        else:
+            print(f"Status unchanged. Waiting for {600 - int(time_since_change)} seconds before stopping Amp.")
+
+    elif new_status and is_playing:
+        # If the status remains unchanged but is currently playing
+        print("Status unchanged. Amp is already playing.")
+
+
     time.sleep(3)
