75961e09f2e151158d6bc1a3d8691115f27cca30
Julian <julian.limburg@gmx.de>
2023-07-23 15:48:10 +0200

    json decoding

--- amp-ctrl.py
+++ amp-ctrl.py
@@ -1,5 +1,26 @@
 import requests
-x = requests.get("http://127.0.0.1:81/api/player/status")
+import json
 
-if x.status_code == 200:
-    print(x.content)
+def check_players_playing(json_data):
+    # Decode the JSON data
+    data = json.loads(json_data)
+    
+    # Get the list of players from the JSON data
+    players = data.get('players', [])
+    
+    # Check if any player is playing
+    for player in players:
+        if player.get('state') == 'playing':
+            return True
+    
+    return False
+
+def request_status():
+    x = requests.get("http://127.0.0.1:81/api/player/status")
+
+    if x.status_code == 200:
+        print(x.content)
+        result = check_players_playing(x.json)
+        print(result)
+
+request_status()
