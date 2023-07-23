22bfc201029a6844ec2a79a6100ed102d9d0945f
Julian <julian.limburg@gmx.de>
2023-07-23 16:40:42 +0200

    absolute path

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-ln -s amp-ctrl-init /etc/init.d/amp-ctrl-init
+ln -s /dev/hifiberry-amp-ctrl/amp-ctrl-init /etc/init.d/amp-ctrl-init
 chmod 755 /etc/init.d/amp-ctrl-init
 update-rc.d amp-ctrl-init defaults
